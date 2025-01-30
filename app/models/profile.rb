1feecff6817d6c49fa6c4a102f29ba34bea72430
milkeclair <milkeclair.black@gmail.com>
2025-01-30 22:09:09 +0900

    feat: バリデーション

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,6 @@
 class Profile < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_one :profile, dependent: :destroy
+  has_many :credentials, dependent: :destroy
+
+  validates :webauthn_id, presence: true, uniqueness: true
+
+  after_initialize do
+    self.webauthn_id ||= ::WebAuthn.generate_user_id
+    while User.exists?(webauthn_id:)
+      self.webauthn_id = ::WebAuthn.generate_user_id
+    end
+  end
 end
